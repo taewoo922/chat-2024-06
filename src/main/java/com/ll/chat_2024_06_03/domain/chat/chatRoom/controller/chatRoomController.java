14b16b737479619699a37855eedbc36ac2892b29
taewoo922 <kimtaewoo922@gmail.com>
2024-06-03 14:25:08 +0900

    chatRoomController

---
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
@@ -0,0 +1,15 @@
+package com.ll.chat_2024_06_03.domain.chat.chatRoom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class chatRoomController {
+    @GetMapping("/chat/room/1")
+    @ResponseBody
+    public String showRoom() {
+        return "1번 채팅방 입니다.";
+    }
+
+}
